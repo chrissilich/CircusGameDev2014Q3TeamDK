fc872a91608ec086e6259ae79ded1ac7815418f9
Shaw Gravitt <smgravitt@gmail.com>
2014-09-05 16:56:26 -0400

    Revert 27dfe18..334115f
    This rolls back to commit 27dfe18356fe43fd286586c759d9d030027e6924.

---
+++ game-main/Assets/scripts/addGemsToScreen.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var YourTexture : GUITexture;
+ 
+function OnTriggerEnter2D(hit : Collider2D){
+    if(hit.gameObject.tag == "Player")
+        {
+            YourTexture.enabled = true; 
+            // yield WaitForSeconds(3);
+            // YourTexture.enabled = false;
+        }
+}
--- game-main/Assets/scripts/cameraShake.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-var objToShake : Transform;
-
-function Update(){
-	var randNrx = Random.Range(0.7,-0.7);
-	var randNry = Random.Range(0.2,-0.2);
-	objToShake.transform.position += Vector2(randNrx, randNry);
-}
