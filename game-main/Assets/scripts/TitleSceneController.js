e21bd3b4fed8b1470492a55b23d79e0e17eb6261
Shaw Gravitt <smgravitt@gmail.com>
2014-09-11 15:35:45 -0400

    updated everything without the movie

--- game-main/Assets/scripts/TitleSceneController.js
+++ game-main/Assets/scripts/TitleSceneController.js
@@ -5,7 +5,7 @@
 var playButtonTexture: Texture;
 
 function OnGUI () {
-	var playButton = GUI.Button( Rect(10, 300, 250, 250), playButtonTexture, GUIStyle.none );
+	var playButton = GUI.Button( Rect(10, 600, 250, 250), playButtonTexture, GUIStyle.none );
 	
 	if (playButton) {
 		Debug.Log("clicked");
